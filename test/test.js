cfdfb68abbf3192fd9a15d5b40cb9645f999ef77
Mario <mario.c.mendes1985@gmail.com>
2015-06-17 21:05:24 +0100

    adds first tests for server

--- test/test.js
+++ test/test.js
@@ -1,3 +1,33 @@
 /**
  * Created by mario (https://github.com/hyprstack) on 07/06/15.
  */
+
+var Code = require('code');
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var after = lab.after;
+var expect = Code.expect;
+
+var server = require("../");
+
+describe("Landing page", function () {
+    it("returns a 200 statusCode and an html file", function (done) {
+        var options = {
+            method: "GET",
+            url: "/"
+        };
+
+        server.inject(options, function(response) {
+            var result = response.result;
+
+            expect(response.statusCode).to.equal(200);
+            expect(result).contains("<!DOCTYPE html>");
+            expect(result).contains("This is a test page!");
+            done();
+        });
+    });
+});
