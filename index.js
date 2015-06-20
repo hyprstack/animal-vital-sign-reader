deaef80f9dd1127c0f5c0c609b143847efbcad15
Mario <mario.c.mendes1985@gmail.com>
2015-06-20 17:29:36 +0100

    adds files and folder for landing page css. updates index.html. updates index.js. updates routes.js

--- index.js
+++ index.js
@@ -5,11 +5,21 @@
 
 var Hapi = require('hapi');
 var rs = require('./lib/routes/routes.js');
+var Path = require('path');
 
 
 var server = new Hapi.Server();
 
 server.connection({port: process.env.PORT || 8080});
+
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './lib/templates',
+    layoutPath: './lib/templates'
+});
 
 server.route(rs);
 
