c0b17d99f16fab4e4137c0973536af325e1c87c6
Mario <mario.c.mendes1985@gmail.com>
2015-07-04 17:11:46 +0100

    adds first version of user authentication adding validation function to validate the session of the cookie on each request

--- index.js
+++ index.js
@@ -8,24 +8,31 @@ var Rs = require('./lib/routes/routes.js');
 var AuthCookie = require('hapi-auth-cookie');
 var Joi = require('joi');
 
+// Our fake User database - TODO: replace with real database once user authentication is working
+var users = {
+    mario: {
+        password: 'test',
+        name: 'mario'
+    }
+};
 
-//var validate = function (request, reply) {
-//
-//    var username = request.payload.username;
-//    var password = request.payload.password;
-//
-//    var user = users[username];
-//    var isValid = user && user.password === password;
-//
-//    if (!isValid) {
-//        return reply().redirect('/login');
-//    }
-//
-//    var credentials = { name: user.name } // Will be accessible in request.auth.credentials
-//
-//    request.auth.session.set(credentials);
-//    return reply('Logged In');
-//};
+var validate = function (request, reply) {
+
+    var username = request.payload.username;
+    var password = request.payload.password;
+
+    var user = users[username];
+    var isValid = user && user.password === password;
+
+    if (!isValid) {
+        return reply().redirect('/login');
+    }
+
+    var credentials = { name: user.name } // Will be accessible in request.auth.credentials
+
+    request.auth.session.set(credentials);
+    return reply('Logged In');
+};
 
 
 var server = new Hapi.Server();
